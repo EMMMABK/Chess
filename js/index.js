e80b83cabf2ad8b93a635fc65bd8adfe85520b81
EMMMABK <adilbikiev2@gmail.com>
2023-12-12 22:44:06 +0600

    react app

--- js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const element  = document.createElement("div");
-    element.style.color = "#769656";
-    element.style.width = "200px";
-    element.style.height = "200px";
-
-    document.getElementsByTagName("div")[0].appendChild(element)
-})
