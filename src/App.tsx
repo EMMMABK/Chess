b7ea6008064195cb95cc4efd38548fe13e5146ad
EMMMABK <adilbikiev2@gmail.com>
2023-12-12 23:01:17 +0600

    add Field

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BoardComponent from './components/BoardComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <BoardComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CellComponent(props) {
+    return (
+        <div className='cell'>
+            
+        </div>
+    );
+}
+
+export default CellComponent;
