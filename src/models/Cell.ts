992782a93d5e279a6a58ed5862b65eed338dfa92
EMMMABK <adilbikiev2@gmail.com>
2023-12-16 20:16:44 +0600

    add models

---
+++ src/models/Cell.ts
@@ -0,0 +1,22 @@
+import { Board } from './Board';
+import { Figure } from './figures/Figure';
+import { Colors } from './Colors';
+export class Cell{
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board: Board;
+    available: boolean;
+    id: number;
+    
+    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null){
+        this.x = x
+        this.y = y
+        this.color = color; 
+        this.figure =figure;
+        this.board = board;
+        this.available = false;
+        this.id = Math.random()      
+    }
+}
