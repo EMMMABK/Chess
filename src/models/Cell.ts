9dfe583159454530600d66cf7c64092350e5b65d
EMMMABK <adilbikiev2@gmail.com>
2023-12-17 15:08:36 +0600

    add All Turns

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -24,6 +24,13 @@ export class Cell{
         return this.figure === null
     }
 
+    isEnemy(target: Cell): boolean {
+        if(target.figure){
+            return this.figure?.color !== target.figure.color;
+        }
+        return false;
+    }
+
     isEmptyVertical(target: Cell): boolean{
         if(this.x !== target.x){
             return false;
